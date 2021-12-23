9b6ff5e8e2e2e50ec0dd85e71e9ad006a333228c
andhikayusup <andeeka.ym@gmail.com>
2021-12-23 12:54:24 +0700

    init project

---
+++ src/test/java/com/andhikayusup/moviex/MoviexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andhikayusup.moviex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
